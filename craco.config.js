83a62cbc1db02022d1568e1bb5c6bf612ba4be88
shonentropy3 <qwerdfkkt@163.com>
2023-09-18 17:19:06 +0800

    Add files via upload

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const CracoAntDesignPlugin = require('craco-antd');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
